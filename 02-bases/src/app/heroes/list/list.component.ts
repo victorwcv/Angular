dee37e5098318a903cf0b403f810710a4a257725
William <victorwccv@gmail.com>
2024-11-20 15:45:59 -0500

    Directivas ngIf- ngFor

--- 02-bases/src/app/heroes/list/list.component.ts
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
+  public heroNames: string[] = ['Spiderman', 'Hulk', 'Thor', 'Antman'];
+  public deletedHero?: string;
 
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+  }
 }
