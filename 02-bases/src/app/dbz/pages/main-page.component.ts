ed1746fe9dc5907744d6346526efcb3d993dc5b5
William <victorwccv@gmail.com>
2024-11-21 16:25:19 -0500

    ngFor propiedades: i, first , last, even, odd

--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
-  templateUrl: './main-page.component.html'
+  templateUrl: './main-page.component.html',
 })
-
 export class MainPageComponent {
-  constructor() { }
+  public characters: Character[] = [
+    {
+      name: 'Krillin',
+      power: 1000,
+    },
+    {
+      name: 'Goku',
+      power: 9500,
+    },
+    {
+      name: 'Vegueta',
+      power: 7500,
+    }
+  ];
 
-  
+  constructor() {}
 }
