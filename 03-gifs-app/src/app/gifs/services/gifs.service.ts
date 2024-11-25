73babba915bafed49d75cf73e3fe0484172cacf6
William <victorwccv@gmail.com>
2024-11-25 14:19:41 -0500

    Create interface for response of fetch

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,14 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
   // Properties
+  public gifList: Gif[] = [];
+
   private _tagsHistory: string[] = [];
   private apiKey: string = 'TG6abiWS5eo5qgnMQmvGsXRWBQEg9QGs';
   private url: string = 'https://api.giphy.com/v1/gifs';
@@ -38,11 +41,14 @@ export class GifsService {
     const params = new HttpParams()
       .set('api_key', this.apiKey)
       .set('limit', '10')
-      .set('q', tag)
+      .set('q', tag);
 
     // fetching data with http
-    this.http.get(`${this.url}/search`, { params }).subscribe((res) => {
-      console.log(res);
-    });
+    this.http
+      .get<SearchResponse>(`${this.url}/search`, { params })
+      .subscribe((res) => {
+        this.gifList = res.data;
+        console.log(this.gifList);
+      });
   }
 }
