cf49e2656aec03167a89e56495d13ab2ee87ab98
William <victorwccv@gmail.com>
2024-11-25 18:35:28 -0500

    Create environments to keep apikeys locally

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Gif, SearchResponse } from "../interfaces/gifs.interfaces";
+import { environment } from "../../../.environments/environment";
 
 @Injectable({
   providedIn: "root",
@@ -10,8 +11,8 @@ export class GifsService {
   public gifList: Gif[] = [];
 
   private _tagsHistory: string[] = [];
-  private apiKey: string = "TG6abiWS5eo5qgnMQmvGsXRWBQEg9QGs";
-  private url: string = "https://api.giphy.com/v1/gifs";
+  private apiKey: string = environment.apikey;
+  private url: string = environment.endpoint;
 
   // Constructor
   constructor(private http: HttpClient) {
