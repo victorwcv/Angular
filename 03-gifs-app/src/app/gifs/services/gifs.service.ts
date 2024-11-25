a0e7c2b55aef68ab16bdfc179974833e72a8a852
William <victorwccv@gmail.com>
2024-11-25 12:42:02 -0500

    Add validations to gifsService

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GifsService {
-
   private _tagsHistory: string[] = [];
 
-  constructor() { }
+  constructor() {}
 
   get tagsHistory() {
-    return [...this._tagsHistory]
+    return [...this._tagsHistory];
+  }
+
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase().trim();
+
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this.tagsHistory.slice(0, 10);
   }
 
   searchTag(tag: string): void {
-    this._tagsHistory.unshift(tag);
-    console.log(this.tagsHistory);
     
+    if (tag.length <= 1) return;
+
+    this.organizeHistory(tag);
   }
-  
 }
