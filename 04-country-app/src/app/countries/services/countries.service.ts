f65d9e27ffc45d7747af32515775e516fa466874
William <victorwccv@gmail.com>
2024-11-30 23:16:30 -0500

    feat: error handler added to countries service

--- 04-country-app/src/app/countries/services/countries.service.ts
+++ 04-country-app/src/app/countries/services/countries.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   searchCapital(term: string): Observable<Country[]> {
     const url: string = `${this.apiUrl}/capital/${term}`;
 
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError( () => of([]) )
+      );
   }
 }
