006e6c5c7c0f044a0e1da1504e95df84151c24f7
William <victorwccv@gmail.com>
2024-12-17 22:34:12 -0500

    feat: i18nSelect Pipe

--- 05-pipe-app/src/app/products/pages/commons-page/commons-page.component.ts
+++ 05-pipe-app/src/app/products/pages/commons-page/commons-page.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-commons-page',
-  templateUrl: './commons-page.component.html'
+  templateUrl: './commons-page.component.html',
 })
 export class CommonsPageComponent {
+  // i18 Select
+  name: string = 'Fernando';
+  gender: 'male' | 'female' = 'male';
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
+  changeClient(): void {
+    this.name = 'Melisa';
+    this.gender = 'female';
+  }
 }
