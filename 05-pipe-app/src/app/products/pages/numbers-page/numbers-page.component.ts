2f79f28094eacc073f4709f150ff8bc27ec52fc4
William <victorwccv@gmail.com>
2024-12-17 22:17:58 -0500

    feat: number pipes

--- 05-pipe-app/src/app/products/pages/numbers-page/numbers-page.component.ts
+++ 05-pipe-app/src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  templateUrl: './numbers-page.component.html'
+  templateUrl: './numbers-page.component.html',
 })
 export class NumbersPageComponent {
-
+  totalSells: number = 345346.657;
+  percentNumber: number = 0.4578;
 }
