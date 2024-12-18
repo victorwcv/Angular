30ead4b58b604d9d5fba602fdf396f812ff5888d
William <victorwccv@gmail.com>
2024-12-17 21:26:42 -0500

    feat: install primeflex and create date pipe examples

--- 05-pipe-app/src/app/products/pages/basics-page/basics-page.component.ts
+++ 05-pipe-app/src/app/products/pages/basics-page/basics-page.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basics-page',
-  templateUrl: './basics-page.component.html'
+  templateUrl: './basics-page.component.html',
 })
 export class BasicsPageComponent {
-
-  nameLower: string = 'fernando'
-  nameUpper: string = 'GONZALEZ'
-  nameTitle: string = 'esTO es UN TItulO'
-
+  nameLower: string = 'fernando';
+  nameUpper: string = 'GONZALEZ';
+  nameTitle: string = 'esTO es UN TItulO';
+  customDate: Date = new Date();
 }
